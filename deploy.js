5b95b636c8acebcc23fb0e51fa0b3308c98da059
ankitkhare <khareankit0804@gmail.com>
2022-09-19 18:23:30 +0530

    Blockchain simple storage

---
+++ deploy.js
@@ -0,0 +1,37 @@
+const ethers = require("ethers")
+const fs = require("fs")
+require("dotenv").config()
+
+async function main() {
+    const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf8")
+    let provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+    let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+        encryptedJson,
+        process.env.PRIVATE_KEY_PASSWORD
+    )
+    wallet = wallet.connect(provider)
+    const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8")
+    const binary = fs.readFileSync(
+        "./SimpleStorage_sol_SimpleStorage.bin",
+        "utf8"
+    )
+
+    const contractFactory = new ethers.ContractFactory(abi, binary, wallet)
+    console.log("Deploying.....")
+    const contract = await contractFactory.deploy()
+    const txReceipt = await contract.deployTransaction.wait(1)
+    console.log(`Contract Address: ${contract.address}`)
+    const currentFavNumber = await contract.retrieve()
+    console.log(`Current fav number: ${currentFavNumber.toString()}`)
+    const transactionResponse = await contract.store(83)
+    const txRecipt = await transactionResponse.wait(1)
+    const updatedFavNumber = await contract.retrieve()
+    console.log(`Updated fav number: ${updatedFavNumber.toString()}`)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
